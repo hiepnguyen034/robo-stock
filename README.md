5547810649083aa26d6ea1118f8874e23599e97c
hiepnguyen034 <hiepnguyen034@gmail.com>
2019-02-19 04:37:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+solution for project 3
---
+++ app/robo_advisor.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+import json
+import os
+import requests
+import pandas as pd 
+from pathlib import Path
+home = str(Path.home())
+
+load_dotenv()
+
+api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
+
+while True:
+	symbol=input('Enter name of stock you want: ')
+	if not symbol.isalpha():
+		print('please make sure to enter name of stock price')
+	else:
+		data=requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+symbol+'&apikey='+api_key)
+
+		if 'Error' in data.text:
+			print('The stock you are looking for is not here')
+		else:
+			break
+
+
+
+j=data.json()
+
+time,opening,high,low,close,volume=[],[],[],[],[],[]
+for key, value in j['Time Series (Daily)'].items():
+    time.append(key)
+    opening.append(value['1. open'])
+    high.append(value['2. high'])
+    low.append(value['3. low'])
+    close.append(value['4. close'])
+    volume.append(value['5. volume'])
+
+result = pd.DataFrame(
+{'time':time,
+'open':opening,
+'high': high,
+ 'low': low,
+ 'close': close,
+ 'volume': volume})
+
+result.to_csv('result.csv')
